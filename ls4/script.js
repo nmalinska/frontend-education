a3f535a9d7a2c41577c3533d719de1c5a8cf13e1
holmanskih <evg.holmanskih@gmail.com>
2021-09-15 19:00:43 +0300

    add js function logic

---
+++ ls4/script.js
@@ -0,0 +1,17 @@
+// создание переменной (константы)
+const pi = 3.14
+
+// вывод на екран
+console.log(pi);
+
+const a = 5
+const res = pi + a
+console.log(res);
+
+function f(x) { 
+    return x
+}
+
+const pointA = f(2)
+
+console.log(pointA);
